9bee5e3cd2c1871c5855f7dc3ee3e3ca1079ca9a
raymondsze <szer0601@gmail.com>
2017-10-10 11:39:40 +0800

    resolve #7 add init script

---
+++ packages/create-react-scripts/scripts/init.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+module.exports = require('react-scripts/scripts/init');
